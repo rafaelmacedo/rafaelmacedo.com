0408d39a7cfadd9fa863834f01b7c9e2a71ef312
Rafael Macedo <macedo.rafaelfernandes@gmail.com>
2014-02-25 18:26:36 -0300

    fix hook

--- lib/git_hook.rb
+++ lib/git_hook.rb
@@ -5,7 +5,7 @@ class GitHook < Sinatra::Base
   def self.parse_git
     sha1, date = `git log HEAD~1..HEAD --pretty=format:%h^%ci`.strip.split("^")
     set :commit_hash, sha1
-    set :commit_date, Time.parse(date)
+    set :commit_date, Time.parse(date) if date
   end
 
   set(:autopull) { production? }
